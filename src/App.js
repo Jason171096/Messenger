ea5701a125f359b853ed234bd194f26e1a3926c7
Jason171096 <bboyace10@gmail.com>
2021-07-06 17:55:28 -0500

    Firebase intregation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import { Button, FormControl, Input, InputLabel } from '@material-ui/core'
+import SendIcon from '@material-ui/icons/Send';
+import { IconButton } from '@material-ui/core';
 import Message from './Message';
 import db from './Firebase'
 import firebase from 'firebase';
@@ -28,13 +30,13 @@ function App() {
       timestamp: firebase.firestore.FieldValue.serverTimestamp()
     })
 
-    setMessages([...messages, { username: username, text: input }])
+    // setMessages([...messages, { username: username, text: input }])
     setInput('')
   }
 
   useEffect(() => {
-    db.collection('messages').orderBy('timestamp').onSnapshot(snapshot => {
-      setMessages(snapshot.docs.map(doc => doc.data()))
+    db.collection('messages').orderBy('timestamp', 'desc').onSnapshot(snapshot => {
+      setMessages(snapshot.docs.map(doc => ({id: doc.id, data: doc.data()})))
     })
   }, [])
 
@@ -44,17 +46,21 @@ function App() {
 
   return (
     <div className="App">
+      <img src="https://logospng.org/download/telegram/logo-telegram-4096.png" width="100px" height="100px"/>
       <h1>Hello Programmers!!</h1>
       <h2>Welcome {username}</h2>
-      <FormControl>
-        <InputLabel>Enter a message.. </InputLabel>
-        <Input value={input} onChange={event => setInput(event.target.value)} />
-        <Button disabled={!input} variant='contained' color='primary' type='submit' onClick={sendMessage}>Send Message</Button>
-      </FormControl>
+      <form className="Form">
+        <FormControl className="FormControl">
+          <Input className="FormInput" placeholder="Enter a message..." value={input} onChange={event => setInput(event.target.value)} />
+          <IconButton className="FormButton" disabled={!input} variant='contained' color='primary' type='submit' onClick={sendMessage}>
+            <SendIcon />
+          </IconButton>
+        </FormControl>
+      </form>
       <FlipMove>
         {
           messages.map(message => (
-            <Message usernameLogin={username} username={message.username} text={message.text} />
+            <Message key={message.id} usernameLogin={username} username={message.data.username} text={message.data.text} />
           ))
         }
       </FlipMove>
